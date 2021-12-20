9e9f7ccc849a71135df4a941da629d71dc9a9585
HarJeet <43493956+harry-dev98@users.noreply.github.com>
2021-12-20 12:44:34 +0000

    added get request for bulk sms

--- main.js
+++ main.js
@@ -15,23 +15,35 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const sendMessage = async (message, numbers) => {
-  return fast2sms.sendMessage({
-    authorization: process.env.SMS_KEY,
-    language: 'unicode',
-    message,
-    numbers: numbers.map((phone) =>
-      phone.startsWith("+91")
-        ? phone.slice(3)
-        : phone.startsWith("0")
-        ? phone.slice(1)
-        : phone
-    ),
-  });
+  return axios.get(
+    `https://www.fast2sms.com/dev/bulkV2?authorization=${
+      process.env.SMS_KEY
+    }&sender_id=TXTIND&message=${message}&route=v3&numbers=${numbers
+      .map((phone) =>
+        phone.startsWith("+91")
+          ? phone.slice(3)
+          : phone.startsWith("0")
+          ? phone.slice(1)
+          : phone
+      )
+      .join(",")}`
+  );
+  // return fast2sms.sendMessage({
+  //   authorization: process.env.SMS_KEY,
+  //   message,
+  //   numbers: numbers.map((phone) =>
+  //     phone.startsWith("+91")
+  //       ? phone.slice(3)
+  //       : phone.startsWith("0")
+  //       ? phone.slice(1)
+  //       : phone
+  //   ),
+  // });
 };
 
 app.post("/api/send-bulk-sms", (req, res) => {
   sendMessage(req.body.message, req.body.numbers).then((response) => {
-    return res.send(response);
+    return res.send(response.data);
   });
 });
 
