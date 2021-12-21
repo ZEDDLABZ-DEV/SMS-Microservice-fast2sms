d37b226a4fa6275a7d1e093e48e44acd8444a2b2
HarJeet <43493956+harry-dev98@users.noreply.github.com>
2021-12-21 12:55:58 +0000

    get manual req

--- main.js
+++ main.js
@@ -36,8 +36,7 @@ const sendMessage = async (message, numbers) => {
 };
 
 app.post("/api/send-bulk-sms", (req, res) => {
-  console.log(encodeURI(req.body.message));
-  sendMessage(encodeURI(req.body.message), req.body.numbers)
+  sendMessage(req.body.message, req.body.numbers)
     .then((response) => {
       return res.send(response.data);
     })
